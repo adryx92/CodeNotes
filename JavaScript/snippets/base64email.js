35b75a64dab9a448c70cebbde103c40ef4a2fe5c
adryx92 <francesco@adragna.xyz>
2019-03-03 17:15:04 +0100

    Doc

--- JavaScript/snippets/base64email.js
+++ JavaScript/snippets/base64email.js
@@ -1,6 +1,8 @@
  function decodeMail() {
-    const STR = '!bWF??pb!H?!?Rv!O?mZ!yYW5??jZXNjb!0?BhZHJ?hZ2!5?!?!?hLnh!5eg=!?!?=!?';
-    let addrTo = atob(STR.split('').filter((c) => {
+    // encoded base64 email with some random ? and !
+    const ENCODED = '!bWF??pb!H?!?Rv!O?mZ!yYW5??jZXNjb!0?BhZHJ?hZ2!5?!?!?hLnh!5eg=!?!?=!?';
+    let decoded = atob(ENCODED.split('').filter((c) => {
         return c == '?' || c == '!' ? '' : c;
     }).join(''));
+    console.log(decoded);
 }
